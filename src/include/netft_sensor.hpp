3640023e886fabd3e4077a5cd8cf93e14fd554fd
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-04 19:34:58 +0100

    Add TCP client socket setup with tiemout

--- src/include/netft_sensor.hpp
+++ src/include/netft_sensor.hpp
@@ -4,6 +4,10 @@
 #include <string>
 #include <array>
 #include <expected>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 struct vec3d {
     double x;
@@ -15,14 +19,24 @@ class netft_sensor {
     private:
         std::string ip;
         int port;
+        double timeout_sec;
         vec3d force_zero_offset{0, 0, 0};
         vec3d torque_zero_offset{0, 0, 0};
         vec3d current_force {0, 0, 0};
         vec3d current_torque {0, 0, 0};
+        bool connected {false};
+        int socket_fd;
 
     public:
-        netft_sensor(const std::string ip, const int port = 49151) : ip(ip), port(port) {
+        netft_sensor(const std::string ip, const int port = 49151, const double timeout_sec = 0.01) : ip(ip), port(port), timeout_sec(timeout_sec) {
+            init();
+        }
 
+        ~netft_sensor() {
+            if(connected) {
+                close(socket_fd);
+                connected = false;
+            }
         }
 
         bool zero_sensor() {
@@ -42,12 +56,55 @@ class netft_sensor {
             return std::pair<vec3d, vec3d>{force_zero_offset, torque_zero_offset};
         }
         
-        bool connect() {
-            
+        bool init() {
+            socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+            if(socket_fd == -1) {
+                connected = false;
+            }
+
+            sockaddr_in server_addr;
+            server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+            server_addr.sin_port = htons(port);
+            server_addr.sin_family = AF_INET;
+
+            if(connect(socket_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+                connected = false;
+            }
+            else {
+                connected = true;
+            }
+
+            timeval timeout;
+            timeout.tv_sec = 0;
+            timeout.tv_usec = timeout_sec * 1e+6;
+            setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+
+            return connected;
         }
 
         std::expected<std::pair<vec3d, vec3d>, std::string> get_data() {
             std::pair<vec3d, vec3d> data {};
+            if(!connected) {
+                return std::unexpected("Not connected yet.");
+            }
+
+            char buffer[1024];
+            ssize_t bytes_received = recv(socket_fd, buffer, sizeof(buffer), 0);
+
+            if(bytes_received == -1) {
+                if(errno == EWOULDBLOCK || errno == EAGAIN) {
+                    return std::unexpected("TImeout occured while reading Force / Torque data.");
+                }
+                else {
+                    return std::unexpected("An error occured while reading Force / Torque data.");
+                }
+            }
+            else if (bytes_received == 0) {
+                connected = false;
+                return std::unexpected("Connection closed by the server.");
+            }
+
             return data;
         }
 };
--- src/tests/sensor_test.cpp
+++ src/tests/sensor_test.cpp
@@ -6,6 +6,10 @@
 int main() {
     netft_sensor* force_sensor = new netft_sensor("10.36.12.7");
     bool success = force_sensor->zero_sensor();
-    std::print("{0}", success)
+    std::print("{0}\n", success);
+    std::print("{0}\n", force_sensor->get_zero_offset().first.x);
+    std::pair<vec3d, vec3d> sensor_data = force_sensor->get_data();
+
+    std::print("Force Z: {0}\n", sensor_data.first.z);
     return 0;
 }
