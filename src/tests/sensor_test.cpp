a5c5a1e7a33b485d411b0bf0183c7c5ca3930bb0
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-04 19:39:38 +0100

    Add value check

--- src/tests/sensor_test.cpp
+++ src/tests/sensor_test.cpp
@@ -8,8 +8,14 @@ int main() {
     bool success = force_sensor->zero_sensor();
     std::print("{0}\n", success);
     std::print("{0}\n", force_sensor->get_zero_offset().first.x);
-    std::pair<vec3d, vec3d> sensor_data = force_sensor->get_data();
+    std::expected<std::pair<vec3d, vec3d>, std::string> sensor_data = force_sensor->get_data();
+    
+    if(sensor_data.has_value()) {
+        std::print("Force Z: {0}\n", sensor_data.value().first.z);
+    }
+    else {
+        std::print("Error while getting force / torque sensor data: {0}", sensor_data.error());
+    }
 
-    std::print("Force Z: {0}\n", sensor_data.first.z);
     return 0;
 }
