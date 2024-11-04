2df8a043daed0ec49e1a32382b2669b4f2db733b
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-04 13:46:23 +0100

    Move to header only structure because this is a small library

--- src/include/netft_sensor.hpp
+++ src/include/netft_sensor.hpp
@@ -4,18 +4,45 @@
 #include <string>
 #include <array>
 
+struct force {
+    double x;
+    double y;
+    double z;
+};
+
+struct torque {
+    double x;
+    double y;
+    double z;
+};
+
 class netft_sensor {
-    private: 
+    private:
         std::string ip;
         int port;
-        std::array<double, 3> F_zero_offset;
-        std::array<double, 3> T_zero_offset;
+        std::array<double, 3> force_zero_offset{0, 0, 0};
+        std::array<double, 3> torque_zero_offset{0, 0, 0};
+        force current_force {};
+        torque current_torque {};
 
     public:
-        netft_sensor(std::string ip, int port = 49151);
-        void zero_sensor();
-        std::pair<double[3], double[3]> get_zero_offset();
-        bool connect();
+        netft_sensor::netft_sensor(std::string ip, int port = 49151) : ip(ip), port(port) {
+
+        }
+        void zero_sensor() {
+            sensor_data = this->get_data();
+            force_zero_offset = sensor_data.force;
+            torque_zero_offset = sensor_data.torque;
+        }
+        std::pair<double[3], double[3]> get_zero_offset() {
+
+        }
+        
+        bool connect() {
+
+        }
+
+        
 };
 
 #endif
--- src/netft_sensor.cpp
+++
@@ -1,8 +0,0 @@
-#include "include/netft_sensor.hpp"
-
-netft_sensor::netft_sensor(std::string ip, int port) : ip(ip), port(port) {
-    for(std::size_t i = 0; i < this->F_zero_offset.size(); i++) {
-        this->F_zero_offset[i] = 0;
-        this->T_zero_offset[i] = 0;
-    }
-}
