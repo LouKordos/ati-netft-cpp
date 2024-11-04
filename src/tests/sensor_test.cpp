58dc17daf82adb91a67c06e8faf8f3a174e96d27
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-04 17:32:50 +0100

    Tell CMake the project language because it's header only library

--- src/tests/sensor_test.cpp
+++ src/tests/sensor_test.cpp
@@ -1,9 +1,11 @@
 #include <string>
 #include <iostream>
 #include "../include/netft_sensor.hpp"
+#include <print>
 
 int main() {
     netft_sensor* force_sensor = new netft_sensor("10.36.12.7");
-
+    bool success = force_sensor->zero_sensor();
+    std::print("{0}", success)
     return 0;
 }
