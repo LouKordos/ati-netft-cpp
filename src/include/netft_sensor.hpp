949aa7f709d6bd28041877e3fdfdb01aceab231e
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-08 12:14:39 +0100

    Use std::array<double, 3> instead of vec3d struct for easier parsing and loop usage

--- src/include/netft_sensor.hpp
+++ src/include/netft_sensor.hpp
@@ -14,20 +14,17 @@
 #include <cstring>
 #include <tracy/Tracy.hpp>
 
-struct vec3d {
-    double x;
-    double y;
-    double z;
-};
+class netft_sensor
+{
 
-class netft_sensor {
-
-    struct message {
+    struct message
+    {
         uint8_t command;
         uint8_t reserved[19];
     };
     // https://www.ati-ia.com/app_content/documents/9620-05-NET%20FT.pdf
-    struct calibration_info {
+    struct calibration_info
+    {
         uint16_t header;
         uint8_t force_units;
         uint8_t torque_units;
@@ -36,232 +33,221 @@ class netft_sensor {
         uint16_t scaleFactors[6];
     };
 
-    private:
-        std::string ip;
-        int port;
-        double timeout_sec;
-        vec3d force_zero_offset{0, 0, 0};
-        vec3d torque_zero_offset{0, 0, 0};
-        vec3d current_force {0, 0, 0};
-        vec3d current_torque {0, 0, 0};
-        bool connected {false};
-        int socket_fd;
-        calibration_info calibration_data;
+private:
+    std::string ip;
+    int port;
+    double timeout_sec;
+    std::array<double, 3> force_zero_offset{0, 0, 0};
+    std::array<double, 3> torque_zero_offset{0, 0, 0};
+    std::array<double, 3> current_force{0, 0, 0};
+    std::array<double, 3> current_torque{0, 0, 0};
+    bool connected{false};
+    int socket_fd;
+    calibration_info calibration_data;
 
-    public:
-        netft_sensor(const std::string ip, const int port = 49151, const double timeout_sec = 0.01) : ip(ip), port(port), timeout_sec(timeout_sec) {
+public:
+    netft_sensor(const std::string ip, const int port = 49151, const double timeout_sec = 0.01) : ip(ip), port(port), timeout_sec(timeout_sec)
+    {
+    }
 
+    ~netft_sensor()
+    {
+        if (connected)
+        {
+            close(socket_fd);
+            connected = false;
         }
+    }
 
-        ~netft_sensor() {
-            if(connected) {
-                close(socket_fd);
-                connected = false;
-            }
-        }
-
-        bool zero_sensor() {
-            std::expected<std::pair<vec3d, vec3d>, std::string> sensor_data = this->get_data(true);
-            if(sensor_data.has_value()) {
-                force_zero_offset = sensor_data.value().first;
-                torque_zero_offset = sensor_data.value().second;
-
-                return true;
-            }
-            else  {
-                return false;
-            }
-        }
-
-        std::pair<vec3d, vec3d> get_zero_offset() {
-            return std::pair<vec3d, vec3d>{force_zero_offset, torque_zero_offset};
-        }
-        
-        bool init() {
-            socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-            if(socket_fd == -1) {
-                connected = false;
-            }
-
-            sockaddr_in server_addr;
-            server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
-            server_addr.sin_port = htons(port);
-            server_addr.sin_family = AF_INET;
-
-            if(connect(socket_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-                connected = false;
-            }
-            else {
-                connected = true;
-            }
-
-            timeval timeout;
-            timeout.tv_sec = 0;
-            timeout.tv_usec = timeout_sec * 1e+6;
-            setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-
-            return connected;
-        }
-
-        bool calibrate() {
-            ZoneScoped;
-            if(!connected) {
-                return false;
-            }
-
-            message get_cal_info_msg {}; // See https://www.ati-ia.com/app_content/documents/9620-05-NET%20FT.pdf
-            get_cal_info_msg.command = 1;
-
-            if(send(socket_fd, &get_cal_info_msg, sizeof(get_cal_info_msg), 0) == -1) {
-                return false;
-            }
-
-            uint8_t buffer[1024];
-            ssize_t bytes_received = recv(socket_fd, buffer, sizeof(buffer), 0);
-
-            size_t expected_size = sizeof(uint16_t) * 2 + sizeof(uint8_t) * 2 + sizeof(uint32_t) * 2 + sizeof(uint16_t) * 2;
-
-            if(bytes_received == -1 || static_cast<size_t>(bytes_received) < expected_size) {
-                return false;
-            }
-            // Connection closed
-            else if (bytes_received == 0) {
-                connected = false;
-                return false;
-            }
-
-            std::span<uint8_t> buffer_span(buffer, bytes_received);
-            size_t offset = 0;
-
-            uint16_t header_net;
-            std::memcpy(&header_net, &buffer_span[offset], sizeof(header_net));
-            calibration_data.header = ntohs(header_net);
-            offset += sizeof(header_net);
-
-            calibration_data.force_units = buffer_span[offset];
-            offset += sizeof(uint8_t);
-            calibration_data.torque_units = buffer_span[offset];
-            offset += sizeof(uint8_t);
-
-            uint32_t counts_per_force_net;
-            std::memcpy(&counts_per_force_net, &buffer_span[offset], sizeof(counts_per_force_net));
-            calibration_data.countsPerForce = ntohl(counts_per_force_net);
-            offset += sizeof(counts_per_force_net);
-
-            uint32_t counts_per_torque_net;
-            std::memcpy(&counts_per_torque_net, &buffer_span[offset], sizeof(counts_per_torque_net));
-            calibration_data.countsPerTorque = ntohl(counts_per_torque_net);
-            offset += sizeof(counts_per_torque_net);
-
-            for(int i = 0; i < 6; i++) {
-                uint16_t scale_factor_net;
-                std::memcpy(&scale_factor_net, &buffer_span[offset], sizeof(scale_factor_net));
-                calibration_data.scaleFactors[i] = ntohs(scale_factor_net);
-                offset += sizeof(scale_factor_net);
-            }
+    bool zero_sensor()
+    {
+        std::expected<std::pair<std::array<double, 3>, std::array<double, 3>>, std::string> sensor_data = this->get_data(true);
+        if (sensor_data.has_value())
+        {
+            force_zero_offset = sensor_data.value().first;
+            torque_zero_offset = sensor_data.value().second;
 
             return true;
         }
-
-        std::expected<std::pair<vec3d, vec3d>, std::string> get_data(bool without_zero_offset = false) {
-            ZoneScoped;
-            if(!connected) {
-                return std::unexpected("Not connected yet.");
-            }
-
-            message get_data_msg {}; // See https://www.ati-ia.com/app_content/documents/9620-05-NET%20FT.pdf
-            {
-                ZoneScopedN("Send READFT msg");
-                if(send(socket_fd, &get_data_msg, sizeof(get_data_msg), 0) == -1) {
-                    return std::unexpected("get_data_msg send failed.");
-                }
-            }
-
-            uint8_t buffer[1024];
-            ZoneNamedN(recvzone, "Recv", true);
-            ssize_t bytes_received = recv(socket_fd, buffer, sizeof(buffer), 0);
-            ZoneNamedN(lengthcheckzone, "Length check", true);
-            size_t expected_size = sizeof(uint16_t) * 2 + sizeof(int16_t) * 6;
-
-            if(bytes_received == -1) {
-                if(errno == EWOULDBLOCK || errno == EAGAIN) {
-                    return std::unexpected("Timeout occured while reading Force / Torque data.");
-                }
-                else {
-                    return std::unexpected("An error occured while reading Force / Torque data.");
-                }
-            }
-            else if(bytes_received == 0) {
-                connected = false;
-                return std::unexpected("Connection closed by the server.");
-            }
-            else if(static_cast<size_t>(bytes_received) < expected_size) {
-                return std::unexpected(std::format("Not enough bytes received. bytes_received={0}, expected_bytes={1}", bytes_received, expected_size));
-            }
-            ZoneNamedN(parsezone, "Parse", true);
-            std::span<uint8_t> buffer_span(buffer, bytes_received);
-            size_t offset = sizeof(uint16_t) + sizeof(uint16_t); // Skip header and status
-
-            // FORCES
-            uint16_t force_x_net;
-            std::memcpy(&force_x_net, &buffer_span[offset], sizeof(force_x_net));
-            offset += sizeof(force_x_net);
-            // noths returns uint16_t but the actual value is int16, so we need two casts
-            current_force.x = static_cast<double>(static_cast<int16_t>(ntohs(force_x_net))) * static_cast<double>(calibration_data.scaleFactors[0]) / static_cast<double>(calibration_data.countsPerForce);
-            if(!without_zero_offset) {
-                current_force.x -= force_zero_offset.x;
-            }
-
-            int16_t force_y_net;
-            std::memcpy(&force_y_net, &buffer_span[offset], sizeof(force_y_net));
-            offset += sizeof(force_y_net);
-
-            current_force.y = static_cast<double>(static_cast<int16_t>(ntohs(force_y_net))) * static_cast<double>(calibration_data.scaleFactors[1]) / static_cast<double>(calibration_data.countsPerForce);
-            if(!without_zero_offset) {
-                current_force.y -= force_zero_offset.y;
-            }
-
-            int16_t force_z_net;
-            std::memcpy(&force_z_net, &buffer_span[offset], sizeof(force_z_net));
-            offset += sizeof(force_z_net);
-
-            current_force.z = static_cast<double>(static_cast<int16_t>(ntohs(force_z_net))) * static_cast<double>(calibration_data.scaleFactors[2]) / static_cast<double>(calibration_data.countsPerForce);
-            if(!without_zero_offset) {
-                current_force.z -= force_zero_offset.z;
-            }
-
-
-            // TORQUES
-            uint16_t torque_x_net;
-            std::memcpy(&torque_x_net, &buffer_span[offset], sizeof(torque_x_net));
-            offset += sizeof(torque_x_net);
-            // noths returns uint16_t but the actual value is int16, so we need two casts
-            current_torque.x = static_cast<double>(static_cast<int16_t>(ntohs(torque_x_net))) * static_cast<double>(calibration_data.scaleFactors[3]) / static_cast<double>(calibration_data.countsPerTorque);
-            if(!without_zero_offset) {
-                current_torque.x -= torque_zero_offset.x;
-            }
-
-            uint16_t torque_y_net;
-            std::memcpy(&torque_y_net, &buffer_span[offset], sizeof(torque_y_net));
-            offset += sizeof(torque_y_net);
-            // noths returns uint16_t but the actual value is int16, so we need two casts
-            current_torque.y = static_cast<double>(static_cast<int16_t>(ntohs(torque_y_net))) * static_cast<double>(calibration_data.scaleFactors[4]) / static_cast<double>(calibration_data.countsPerTorque);
-            if(!without_zero_offset) {
-                current_torque.y -= torque_zero_offset.y;
-            }
-
-            uint16_t torque_z_net;
-            std::memcpy(&torque_z_net, &buffer_span[offset], sizeof(torque_z_net));
-            offset += sizeof(torque_z_net);
-            // noths returns uint16_t but the actual value is int16, so we need two casts
-            current_torque.z = static_cast<double>(static_cast<int16_t>(ntohs(torque_z_net))) * static_cast<double>(calibration_data.scaleFactors[5]) / static_cast<double>(calibration_data.countsPerTorque);
-            if(!without_zero_offset) {
-                current_torque.z -= torque_zero_offset.z;
-            }
-
-            return std::pair<vec3d, vec3d>{current_force, current_torque};
+        else
+        {
+            return false;
         }
+    }
+
+    std::pair<std::array<double, 3>, std::array<double, 3>> get_zero_offset()
+    {
+        return std::pair<std::array<double, 3>, std::array<double, 3>>{force_zero_offset, torque_zero_offset};
+    }
+
+    bool init()
+    {
+        socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+        if (socket_fd == -1)
+        {
+            connected = false;
+        }
+
+        sockaddr_in server_addr;
+        server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+        server_addr.sin_port = htons(port);
+        server_addr.sin_family = AF_INET;
+
+        if (connect(socket_fd, (sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+        {
+            connected = false;
+        }
+        else
+        {
+            connected = true;
+        }
+
+        timeval timeout;
+        timeout.tv_sec = 0;
+        timeout.tv_usec = timeout_sec * 1e+6;
+        setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+
+        return connected;
+    }
+
+    bool calibrate()
+    {
+        ZoneScoped;
+        if (!connected)
+        {
+            return false;
+        }
+
+        message get_cal_info_msg{}; // See https://www.ati-ia.com/app_content/documents/9620-05-NET%20FT.pdf
+        get_cal_info_msg.command = 1;
+
+        if (send(socket_fd, &get_cal_info_msg, sizeof(get_cal_info_msg), 0) == -1)
+        {
+            return false;
+        }
+
+        uint8_t buffer[1024];
+        ssize_t bytes_received = recv(socket_fd, buffer, sizeof(buffer), 0);
+
+        size_t expected_size = sizeof(uint16_t) * 2 + sizeof(uint8_t) * 2 + sizeof(uint32_t) * 2 + sizeof(uint16_t) * 2;
+
+        if (bytes_received == -1 || static_cast<size_t>(bytes_received) < expected_size)
+        {
+            return false;
+        }
+        // Connection closed
+        else if (bytes_received == 0)
+        {
+            connected = false;
+            return false;
+        }
+
+        std::span<uint8_t> buffer_span(buffer, bytes_received);
+        size_t offset = 0;
+
+        uint16_t header_net;
+        std::memcpy(&header_net, &buffer_span[offset], sizeof(header_net));
+        calibration_data.header = ntohs(header_net);
+        offset += sizeof(header_net);
+
+        calibration_data.force_units = buffer_span[offset];
+        offset += sizeof(uint8_t);
+        calibration_data.torque_units = buffer_span[offset];
+        offset += sizeof(uint8_t);
+
+        uint32_t counts_per_force_net;
+        std::memcpy(&counts_per_force_net, &buffer_span[offset], sizeof(counts_per_force_net));
+        calibration_data.countsPerForce = ntohl(counts_per_force_net);
+        offset += sizeof(counts_per_force_net);
+
+        uint32_t counts_per_torque_net;
+        std::memcpy(&counts_per_torque_net, &buffer_span[offset], sizeof(counts_per_torque_net));
+        calibration_data.countsPerTorque = ntohl(counts_per_torque_net);
+        offset += sizeof(counts_per_torque_net);
+
+        for (int i = 0; i < 6; i++)
+        {
+            uint16_t scale_factor_net;
+            std::memcpy(&scale_factor_net, &buffer_span[offset], sizeof(scale_factor_net));
+            calibration_data.scaleFactors[i] = ntohs(scale_factor_net);
+            offset += sizeof(scale_factor_net);
+        }
+
+        return true;
+    }
+
+    std::expected<std::pair<std::array<double, 3>, std::array<double, 3>>, std::string> get_data(bool without_zero_offset = false)
+    {
+        ZoneScoped;
+        if (!connected)
+        {
+            return std::unexpected("Not connected yet.");
+        }
+
+        message get_data_msg{}; // See https://www.ati-ia.com/app_content/documents/9620-05-NET%20FT.pdf
+        {
+            ZoneScopedN("Send READFT msg");
+            if (send(socket_fd, &get_data_msg, sizeof(get_data_msg), 0) == -1)
+            {
+                return std::unexpected("get_data_msg send failed.");
+            }
+        }
+
+        uint8_t buffer[1024];
+        ZoneNamedN(recvzone, "Recv", true);
+        ssize_t bytes_received = recv(socket_fd, buffer, sizeof(buffer), 0);
+        ZoneNamedN(lengthcheckzone, "Length check", true);
+        size_t expected_size = sizeof(uint16_t) * 2 + sizeof(int16_t) * 6;
+
+        if (bytes_received == -1)
+        {
+            if (errno == EWOULDBLOCK || errno == EAGAIN)
+            {
+                return std::unexpected("Timeout occured while reading Force / Torque data.");
+            }
+            else
+            {
+                return std::unexpected("An error occured while reading Force / Torque data.");
+            }
+        }
+        else if (bytes_received == 0)
+        {
+            connected = false;
+            return std::unexpected("Connection closed by the server.");
+        }
+        else if (static_cast<size_t>(bytes_received) < expected_size)
+        {
+            return std::unexpected(std::format("Not enough bytes received. bytes_received={0}, expected_bytes={1}", bytes_received, expected_size));
+        }
+        ZoneNamedN(parsezone, "Parse buffer", true);
+        std::span<uint8_t> buffer_span(buffer, bytes_received);
+        size_t offset = sizeof(uint16_t) + sizeof(uint16_t); // Skip header and status
+
+        for(int i = 0; i < 3; i++) {
+            ZoneScopedN("Force Parse loop");
+            uint16_t network_value;
+            std::memcpy(&network_value, &buffer_span[offset], sizeof(network_value));
+            offset += sizeof(network_value);
+            current_force[i] = static_cast<double>(static_cast<int16_t>(ntohs(network_value))) * static_cast<double>(calibration_data.scaleFactors[i]) / static_cast<double>(calibration_data.countsPerForce);
+            if(!without_zero_offset) {
+                current_force[i] -= force_zero_offset[i];
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            ZoneScopedN("Torque Parse loop");
+            uint16_t network_value;
+            std::memcpy(&network_value, &buffer_span[offset], sizeof(network_value));
+            offset += sizeof(network_value);
+            current_torque[i] = static_cast<double>(static_cast<int16_t>(ntohs(network_value))) * static_cast<double>(calibration_data.scaleFactors[i+3]) / static_cast<double>(calibration_data.countsPerTorque);
+            if(!without_zero_offset) {
+                current_torque[i] -= torque_zero_offset[i];
+            }
+        }
+
+        return std::pair<std::array<double, 3>, std::array<double, 3>>{current_force, current_torque};
+    }
 };
 
 #endif
--- src/tests/sensor_test.cpp
+++ src/tests/sensor_test.cpp
@@ -5,7 +5,6 @@
 #include <expected>
 #include <tracy/Tracy.hpp>
 
-
 int main() {
     ZoneScoped;
     netft_sensor* force_sensor = new netft_sensor("10.36.12.7");
@@ -13,20 +12,20 @@ int main() {
     bool calibrated = force_sensor->calibrate();
     // bool success = force_sensor->zero_sensor();
     // std::print("connected={0}\tzero_success={1}\tcalibrated={2}\n", connected, success, calibrated);
-    std::print("{0}\n", force_sensor->get_zero_offset().first.x);
+    std::println("force_zero_offset={0}, {1}, {2}\ttorque_zero_offset={3}, {4}, {5}", force_sensor->get_zero_offset().first[0], force_sensor->get_zero_offset().first[1], force_sensor->get_zero_offset().first[2], force_sensor->get_zero_offset().second[0], force_sensor->get_zero_offset().second[1], force_sensor->get_zero_offset().second[2]);
 
     for(int i = 0; i < 10000; i++) {
         FrameMark;
         // ZoneScopedN("Get data");
-        std::expected<std::pair<vec3d, vec3d>, std::string> sensor_data = force_sensor->get_data();
+        std::expected<std::pair<std::array<double, 3>, std::array<double, 3>>, std::string> sensor_data = force_sensor->get_data();
         ZoneNamedN(aftergetdatazone, "Before has_value", true);
         if(sensor_data.has_value()) {
             ZoneNamedN(afterhasvaluecheckzone, "After has_value check", true);
             // ZoneScopedN("Has Value");
-            std::print("Force: {0:.4f} {1:.4f} {2:.4f}\tTorque: {3:.4f} {4:.4f} {5:.4f}\n", sensor_data.value().first.x, sensor_data.value().first.y, sensor_data.value().first.z, sensor_data.value().second.x, sensor_data.value().second.y, sensor_data.value().second.z);
+            std::println("Force [N]: {0:.4f} {1:.4f} {2:.4f}\tTorque [Nm]: {3:.4f} {4:.4f} {5:.4f}", sensor_data.value().first[0], sensor_data.value().first[1], sensor_data.value().first[2], sensor_data.value().second[0], sensor_data.value().second[1], sensor_data.value().second[2]);
         }
         else {
-            std::print("Error while getting force / torque sensor data: {0}\n", sensor_data.error());
+            std::println("Error while getting force / torque sensor data: {0}", sensor_data.error());
             exit(1);
         }
     }
