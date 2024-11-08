c68dd33654579330d921de0f521935b67892d0e4
Loukas K. <loukas.kordos@daimlertruck.com>
2024-11-08 12:20:59 +0100

    Avoid dynamic allocation for force sensor

--- src/tests/sensor_test.cpp
+++ src/tests/sensor_test.cpp
@@ -7,17 +7,17 @@
 
 int main() {
     ZoneScoped;
-    netft_sensor* force_sensor = new netft_sensor("10.36.12.7");
-    bool connected = force_sensor->init();
-    bool calibrated = force_sensor->calibrate();
+    netft_sensor force_sensor("10.36.12.7");
+    bool connected = force_sensor.init();
+    bool calibrated = force_sensor.calibrate();
     // bool success = force_sensor->zero_sensor();
     // std::print("connected={0}\tzero_success={1}\tcalibrated={2}\n", connected, success, calibrated);
-    std::println("force_zero_offset={0}, {1}, {2}\ttorque_zero_offset={3}, {4}, {5}", force_sensor->get_zero_offset().first[0], force_sensor->get_zero_offset().first[1], force_sensor->get_zero_offset().first[2], force_sensor->get_zero_offset().second[0], force_sensor->get_zero_offset().second[1], force_sensor->get_zero_offset().second[2]);
+    std::println("force_zero_offset={0}, {1}, {2}\ttorque_zero_offset={3}, {4}, {5}", force_sensor.get_zero_offset().first[0], force_sensor.get_zero_offset().first[1], force_sensor.get_zero_offset().first[2], force_sensor.get_zero_offset().second[0], force_sensor.get_zero_offset().second[1], force_sensor.get_zero_offset().second[2]);
 
     for(int i = 0; i < 10000; i++) {
         FrameMark;
         // ZoneScopedN("Get data");
-        std::expected<std::pair<std::array<double, 3>, std::array<double, 3>>, std::string> sensor_data = force_sensor->get_data();
+        std::expected<std::pair<std::array<double, 3>, std::array<double, 3>>, std::string> sensor_data = force_sensor.get_data();
         ZoneNamedN(aftergetdatazone, "Before has_value", true);
         if(sensor_data.has_value()) {
             ZoneNamedN(afterhasvaluecheckzone, "After has_value check", true);
